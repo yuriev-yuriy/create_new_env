ff8ec4584f8556c96591a127ef1a6d8a9d506dd7
Yuriev Yuriy <yuriev1986@gmail.com>
2020-12-04 18:19:59 +0200

    hide and show main paige section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-import './sass/styles.scss';
+'use strict';
+// import './sass/styles.scss';
+const galleryImg = [ "./images/slider/gallery-lifestyle-01-970x524.jpg",
+"./images/slider/gallery-lifestyle-03-970x524.jpg",
+"./images/slider/gallery-lifestyle-02-970x524.jpg",
+"./images/slider/gallery-lifestyle-04-970x524.jpg",
+"./images/slider/gallery-lifestyle-05-970x524.jpg",
+"./images/slider/gallery-lifestyle-06-970x524.jpg",
+"./images/slider/gallery-lifestyle-07-970x524.jpg",
+"./images/slider/gallery-lifestyle-08-970x524.jpg",]
+console.log('it works');
+const list = document.querySelector('.main__list');
+const links = document.querySelectorAll('.main__list-item');
+const innerContainer = document.querySelector('.main__inner-container');
+const innerItems = document.querySelectorAll('.main__inner-item');
+const innerImg = document.querySelector('.main__inner-img');
+const innerTitle = document.querySelector('.main__inner-title');
+const innerBtn = document.querySelector('.main__inner-btn');
+
+
+
+links.forEach(link => link.addEventListener('click', event => {
+    const clickedEl = event.currentTarget;
+    list.classList.add('hide');
+    innerTitle.textContent = clickedEl.textContent;
+    innerContainer.classList.remove('hide');
+    })
+)
+
+innerBtn.addEventListener('click', () => {
+    list.classList.remove('hide');
+    innerContainer.classList.add('hide');
+})
+
+// innerItems.forEach(innerItem =>
+//     innerItem.addEventListener('click', event => {
+//         const clickItem = event.currentTarget;
+        // innerImg.src = Object.values(galleryImg);
+            innerItems.forEach(innerItem => {
+                innerItem.addEventListener('click', event => {
+                    const clickedItem = event.currentTarget;
+                    innerImg.src = `${galleryImg}`;
+                    
+}
+            //         console.log(each.src)
+            // innerImg.src = each.src;
+            
+            )})
+        
+        
+        
+              
+
+
