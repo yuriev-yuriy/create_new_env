18e61814193abb7708a355b2f437384ee1206e4f
Yuriev Yuriy <yuriev1986@gmail.com>
2020-12-05 01:01:14 +0200

    changing pic

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const galleryImg = [ "./images/slider/gallery-lifestyle-01-970x524.jpg",
 "./images/slider/gallery-lifestyle-06-970x524.jpg",
 "./images/slider/gallery-lifestyle-07-970x524.jpg",
 "./images/slider/gallery-lifestyle-08-970x524.jpg",]
-console.log('it works');
+
+const headerListRef = document.querySelectorAll('.header__list-item');
+const headerAfterRef = document.querySelectorAll('.kapec');
 const list = document.querySelector('.main__list');
 const links = document.querySelectorAll('.main__list-item');
 const innerContainer = document.querySelector('.main__inner-container');
@@ -17,35 +19,36 @@ const innerImg = document.querySelector('.main__inner-img');
 const innerTitle = document.querySelector('.main__inner-title');
 const innerBtn = document.querySelector('.main__inner-btn');
 
-
+headerListRef.forEach(link1 => link1.addEventListener('click', () => {
+    headerAfterRef.forEach(link2 => {
+        const clickedEl = event.currentTarget;
+        clickedEl.firstElementChild.classList.add('active'),
+            clickedEl.firstElementChild.classList.remove('kapec')
+    },
+    )
+}))
 
 links.forEach(link => link.addEventListener('click', event => {
     const clickedEl = event.currentTarget;
     list.classList.add('hide');
     innerTitle.textContent = clickedEl.textContent;
     innerContainer.classList.remove('hide');
-    })
-)
+    }))
+
 
 innerBtn.addEventListener('click', () => {
     list.classList.remove('hide');
     innerContainer.classList.add('hide');
 })
 
-// innerItems.forEach(innerItem =>
-//     innerItem.addEventListener('click', event => {
-//         const clickItem = event.currentTarget;
-        // innerImg.src = Object.values(galleryImg);
-            innerItems.forEach(innerItem => {
-                innerItem.addEventListener('click', event => {
-                    const clickedItem = event.currentTarget;
-                    // innerImg.src = `${galleryImg}`;
-                    
-}
-            //         console.log(each.src)
-            // innerImg.src = each.src;
-            
-            )})
+galleryImg.forEach(item => {
+    innerItems.forEach(innerItem => {
+        innerItem.addEventListener('click', event => {
+            let numRef = Number(innerItem.id);
+  innerImg.src = galleryImg[numRef];
+        })
+    })
+})
         
         
         
